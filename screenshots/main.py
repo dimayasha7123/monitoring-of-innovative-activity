4cfef8d72521ab551d577088916fa559f9e556f0
dwarf133 <you@example.com>
2022-06-25 04:03:20 +0500

    добавлено создание скриншота элемента страницы

---
+++ screenshots/main.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+company_name = 'МЦЭ-Инжиниринг'
+url = 'https://mcee.ru/tpost/of551zcy81-20-maya-2021'
+
+
+def make_screenshot(company_name: str, url: str) -> bool: 
+    with webdriver.Firefox() as driver:
+        driver.get(url)
+        place = driver.find_element(By.XPATH, f"//*[contains(text(),'{company_name}')]")
+        time.sleep(0.5)
+        place.screenshot(f'./{company_name}_{datetime.now()}.png')
+    return True
+
+make_screenshot(company_name, url)
