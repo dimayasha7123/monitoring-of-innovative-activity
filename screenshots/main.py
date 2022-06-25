2887dfc8159dbe4f468e3fe91a107559d3f1bdd3
dwarf133 <you@example.com>
2022-06-25 23:08:48 +0500

    добавил отправление скриншотов в редис

--- screenshots/main.py
+++ screenshots/main.py
@@ -3,20 +3,36 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import redis
+from PIL import Image
+from io import BytesIO
 
 
 company_name = 'МЦЭ-Инжиниринг'
+company_category = 'красавцы'
 url = 'https://mcee.ru/tpost/of551zcy81-20-maya-2021'
 
+r = redis.StrictRedis(host='localhost', port='6379', db=0)
 
-def make_screenshot(company_name: str, url: str) -> bool: 
+def make_screenshot(company_name: str, company_category: str, url: str) -> bool: 
     with webdriver.Firefox() as driver:
         driver.get(url)
         place = driver.find_element(By.XPATH, f"//*[contains(text(),'{company_name}')]")
         time.sleep(0.5)
-        place.screenshot(f'./{company_name}_{datetime.now()}.png')
+        place_png = place.screenshot_as_png
+        filename = f'./{company_name}_{company_category}_{datetime.now()}.PNG'
+    
+    with BytesIO() as output:
+        im = Image.open(BytesIO(place_png))
+        im.save(output, format='png')
+        r.set(filename, output.getvalue())
+        r.bgsave()
+
     return True
 
-make_screenshot(company_name, url)
+# Code for testing
 
-#screenshot(f'./{company_name}_{datetime.now()}.png')
+make_screenshot(company_name, company_category, url)
+names = r.keys()
+pic = r.get(names[0])
+im = Image.open(BytesIO(pic))
+im.save(names[0], format='png')
